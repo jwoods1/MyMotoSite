4e6a782569c3b4e2a177c97aadaf20e00993eeb9
b87775 <jwoods1@fullsail.edu>
2013-11-10 23:07:07 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyMotoSite::Application.config.secret_key_base = '1e3c0b6b9db8422b31be39119b6d7548b23e90fcfae0311eda99844207ce130a023e47278ece8fd64a7039bbc273a183d2303e7df87d53dd53eaffbca0786206'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyMotoSite::Application.config.session_store :cookie_store, key: '_MyMotoSite_session'
